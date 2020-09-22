78a0ee0e19b9defee252f61bd798a5bb08a2cd6e
yogesh.matta29 <yogesh.matta29@gmail.com>
2020-09-22 15:35:16 +0530

    Updates

---
+++ precache-manifest.7654cc8566b804aca0a841e610caf8e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2acde6203bfce663e93c93bdbc2ee586",
+    "url": "/yogeshmatta29/restlist/index.html"
+  },
+  {
+    "revision": "7fbcb2237e252d2e897d",
+    "url": "/yogeshmatta29/restlist/static/css/2.43079d8c.chunk.css"
+  },
+  {
+    "revision": "9c7e36467e2d77ae0c92",
+    "url": "/yogeshmatta29/restlist/static/css/main.a92cf478.chunk.css"
+  },
+  {
+    "revision": "7fbcb2237e252d2e897d",
+    "url": "/yogeshmatta29/restlist/static/js/2.fce84d4b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/yogeshmatta29/restlist/static/js/2.fce84d4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c7e36467e2d77ae0c92",
+    "url": "/yogeshmatta29/restlist/static/js/main.8964a480.chunk.js"
+  },
+  {
+    "revision": "f5cbc11b88e35c51a7c1",
+    "url": "/yogeshmatta29/restlist/static/js/runtime-main.9c136d43.js"
+  }
+]);
